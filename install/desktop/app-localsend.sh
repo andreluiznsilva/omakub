5c9a9407be32124235c2add322153a78f9756150
Andre Nascimento <andreluiznsilva@gmail.com>
2024-09-19 10:13:50 +0100

    Update arm-support

--- install/desktop/app-localsend.sh
+++ install/desktop/app-localsend.sh
@@ -1,5 +1,6 @@
 ARCH=$(dpkg --print-architecture)
 [ "${ARCH}" == "amd64" ] && ARCH="x86-64"
+[ "${ARCH}" == "arm64" ] && ARCH="arm-64"
 cd /tmp
 LOCALSEND_VERSION=$(curl -s "https://api.github.com/repos/localsend/localsend/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 wget -O localsend.deb "https://github.com/localsend/localsend/releases/latest/download/LocalSend-${LOCALSEND_VERSION}-linux-${ARCH}.deb"
